1c904dff3a915245fbc108b848710b36a8823405
tamikahmatov-boop <tamikahmatov@gmail.com>
2026-06-22 14:20:39 +0300

    Update Flask app to serve static files from frontend

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="frontend")
 
 @app.route("/")
 def home():
-    return "FRONT WORKS"
+    return send_from_directory("frontend", "index.html")
 
 @app.route("/test")
 def test():
     return "OK"
+
+@app.route("/api/pumps")
+def pumps():
+    return []
 
 if __name__ == "__main__":
     app.run()
