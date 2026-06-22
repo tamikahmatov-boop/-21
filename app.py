066dbf81641cd04cb7f0107a4eee455c97a35911
tamikahmatov-boop <tamikahmatov@gmail.com>
2026-06-22 15:21:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-from flask import Flask, send_from_directory, jsonify
-import os
+from flask import Flask
 
-app = Flask(__name__, static_folder="frontend")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return send_from_directory("frontend", "index.html")
+    return "WORKS"
 
 @app.route("/test")
 def test():
     return "OK"
-
-@app.route("/api/pumps")
-def pumps():
-    return jsonify([])
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
